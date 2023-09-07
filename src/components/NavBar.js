68a52d12b9dc74a4c569829623124aa446a4216c
Carla Marina Apata <carla.apata@gmail.com>
2023-09-07 16:25:05 -0300

    Cambio fuente css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,10 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavDropdown title="Productos" id="basic-nav-dropdown" className='m-0 m-lg-2'>
+            <Nav.Link href="#home" className='m-0 m-lg-3 fs-5'>Inicio</Nav.Link>
+            <Nav.Link href="#link" className='m-0 m-lg-3 fs-5'>Quienes somos</Nav.Link>
+            <Nav.Link href="#contacto" className='m-0 m-lg-3 fs-5'>Contactanos</Nav.Link>
+            <NavDropdown title="Productos" id="basic-nav-dropdown" className='m-0 m-lg-3 fs-5'>
               <NavDropdown.Item href="#action/3.1">Cafeína</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Barras Protéicas</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Proteína</NavDropdown.Item>
@@ -20,9 +23,6 @@ function NavBar() {
               <NavDropdown.Item href="#action/3.7">Carnitina</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.8">Ultra Mass</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#home" className='m-0 m-lg-2'>Inicio</Nav.Link>
-            <Nav.Link href="#link" className='m-0 m-lg-2'>Quienes somos</Nav.Link>
-            <Nav.Link href="#contacto" className='m-0 m-lg-2'>Contactanos</Nav.Link>
           </Nav>
           <CartWidget />
         </Navbar.Collapse>
